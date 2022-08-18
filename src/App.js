267f34d7eba29369d7ba61585d3b92069c41bae9
PRINCE KESHARWANI <73026415+Prince1121@users.noreply.github.com>
2022-08-18 19:58:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import Header from './components/Header';
+const App= ()=>{
+
+  return(
+  <div className="container">
+    <div className='app-wrapper'>
+      <div>
+      <Header />
+      </div>
+      
+      </div>
+    
+
+  </div>
+
+  )
+
+}
+
+
+
+export default App;
